6aa4bd34f67dc6e9d269215da01ca6b8881176d1
Rajnishpal003 <Rajnishpal006@gmail.com>
2025-08-07 14:47:55 +0530

    i

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -1,5 +1,5 @@
 import { fetchRequestHandler } from '@trpc/server/adapters/fetch'
-// import { createTRPCContext } from '../../../..//server/context'
+
 import { appRouter } from '../../../../server/index.'
 
 const handler = (req: Request) =>
